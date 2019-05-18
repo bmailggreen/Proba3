7683dc465e3b198f3c67fe17d7264c0c7b569a20
asd <asd@asd.com>
2019-05-18 15:17:29 +0200

    asd

--- Szamologep/src/com/company/Szamologep.java
+++ Szamologep/src/com/company/Szamologep.java
@@ -5,7 +5,7 @@ public class Szamologep {
     public static double osszead(int a, int b) {
         return a + b;
     }
-    public static double hatvanyozas (int a,int b){return Math.pow(a,b);}
+    public static double hatvanyozas (double a,double b){return Math.pow(a,b);}
 
 
 
